3e9804aee727c78e3b5a3018c4d9e1c17cc8f67e
Manuel Midiri <manuel.midiri@hotmail.it>
2021-07-09 10:04:40 +0200

    searchBar component and routing base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './feature/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'jikan', pathMatch: 'full' },
+  { path: 'jikan', component: HomepageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
